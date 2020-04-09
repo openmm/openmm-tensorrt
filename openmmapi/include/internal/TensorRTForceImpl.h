186608d9876816b483c212ac6d75c5acb433f30d
Raimondas Galvelis <r.galvelis@acellera.com>
2020-04-10 00:59:02 +0200

    Implement graph deserialization

--- openmmapi/include/internal/TensorRTForceImpl.h
+++ openmmapi/include/internal/TensorRTForceImpl.h
@@ -5,10 +5,19 @@
 #include "openmm/internal/ForceImpl.h"
 #include "openmm/Kernel.h"
 #include <tensorflow/c/c_api.h>
+#include <NvInfer.h>
+#include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
 
 namespace OpenMM {
+
+class Logger : public nvinfer1:: ILogger {
+    void log(Severity severity, const char* msg) override {
+        std::cout << msg << std::endl;
+    }
+};
 
 /**
  * This is the internal implementation of TensorRTForce.
@@ -30,6 +39,11 @@ private:
     TF_Graph* graph;
     TF_Session* session;
     TF_Status* status;
+    Logger logger;
+    using Runtime = nvinfer1::IRuntime;
+    using Engine = nvinfer1::ICudaEngine;
+    std::shared_ptr<Engine> engine;
+    std::shared_ptr<Runtime> runtime;
 };
 
 } // namespace OpenMM
